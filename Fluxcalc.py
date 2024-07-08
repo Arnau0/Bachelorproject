89091ccdb046539cb669f9211095da55f4a0ff1b
Arnau0 <94105773+Arnau0@users.noreply.github.com>
2024-07-08 12:12:26 +0200

    Add files via upload
    updated the comments in the script. Also converted the flux density values to milliJansky units

--- Fluxcalc.py
+++ Fluxcalc.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math as math
 import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
 
 c = 29979245800  # cm/s
 h = 6.62507015 / (10**27)  # cm^2 g s^-1
@@ -22,7 +21,7 @@ def abs_coeff_r(T, nu, n_0, r):
     T = temperature
     nu = frequency
     n_0 = initial number density profile
-    r = distance to absorption (from observer?) => variable
+    r = distance to absorption => variable
     """
     C = 3.692 * (10**8)  # the  number in front of the expression
     BB = 1 - np.exp(
@@ -32,28 +31,41 @@ def abs_coeff_r(T, nu, n_0, r):
     return (C * BB * g * (n_0**2)) / (np.sqrt(T) * (nu**3) * 4 * (r**4 / R_sun**4))
 
 
+# number of points of a full axis of the system
 L_grid = 500
+
+# range of the system
 lmax = 100 * R_sun
+
+# the array for the y-axis is only for the top half
 x_axis = np.linspace(-lmax, lmax, L_grid)
 y_axis = np.linspace(0, lmax, int(L_grid / 2))
 
-# note the index for wind_coordinate_system: x = [:,0] & y = [:,1]
+
 x, y = np.meshgrid(x_axis, y_axis, indexing="ij")
 r = (x**2 + y**2) ** 0.5  # 2d grid with r vector
 mask = (r < R_sun) | ((x > 0) & (abs(y) < R_sun))
 
 
-def I_tau(tau_max, nu):  # the intensity given a maximum optical depth
+# the intensity given a maximum optical depth
+def I_tau(tau_max, nu):
     B = ((2 * h * nu**3) / (c**2)) * (np.exp((h * nu) / (k_B * exT)) - 1) ** (-1)
     return B * (1 - np.exp(-tau_max))
 
 
+def toMilliJansky(F):
+    return F * int(1e26)
+
+
+# array of frequencies
 frequency = np.linspace(10**6, (10**10), L_grid)
 
+# spatial elements
 dx = x_axis[1] - x_axis[0]
 dy = y_axis[1] - y_axis[0]
 dA = 2 * np.pi * y_axis * dy
 
+# calculate the flux density
 Flux_nu = []
 for nu in frequency:
 
@@ -61,18 +73,21 @@ for nu in frequency:
     alpha_nu = abs_coeff_r(exT, nu, exn_0, r)
     alpha_nu[mask] = 0
 
+    # optical depth
     tau_max = np.sum(alpha_nu, axis=0) * dx
 
     # intensity
     I_nu = I_tau(tau_max, nu)
 
-    # flux
+    # flux (erg s^-1 cm^-2 Hz^-1 = 10^-23 Jansky)
+    # note that the distance of the observer to the system is taken to be 1 pc
     F = np.sum(I_nu * dA) / (pc**2)
+    F = toMilliJansky(F)
     Flux_nu.append(F)
 
 plt.plot(frequency, Flux_nu)
-plt.xlabel("frequency")
-plt.ylabel("Flux")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Flux density (mJy)")
 plt.xscale("log")
 plt.yscale("log")
 plt.savefig("Flux-frequency.png")
