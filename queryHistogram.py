636a2fe561c269200f1489b51954465249f19edf
Arnau0 <94105773+Arnau0@users.noreply.github.com>
2024-08-06 15:13:01 +0200

    Add files via upload
    updated the file, removing redundant lines and adding Mstar and Rstar histograms and R-M plot. Also filtered out stars that have an absolute K magnitude (M_Ks) outside the range for the relations in Mann et al. (2015). As it turns out, quite a number of stars do have a M_Ks outside this range, which is [4.5, 9.5).

--- queryHistogram.py
+++ queryHistogram.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 
 def getMass(M_ks):
     """Calculate the mass of a star using the semi-empirical mass-K magnitude relation from Mann et al. (2015).
+    Eq (10)
     M_ks = K magnitude observed from the star
-    returns mass in stellar radii
+    returns mass in stellar masses
     """
     a = 0.5858
     bX = 0.3872 * M_ks
@@ -15,6 +16,18 @@ def getMass(M_ks):
     dX3 = 0.0106 * M_ks**3
     eX4 = -2.7262e-4 * M_ks**4
     return a + bX + cX2 + dX3 + eX4
+
+
+def getRadius(M_ks):
+    """Calculate radius of a star using semi-empirical radius-K magnitude relation from Mann et al.
+    Eq (4)
+    M_ks = K magnitude observed from the star
+    returns mass in stellar radii
+    """
+    a = 1.9515
+    bX = -0.3520 * M_ks
+    cX2 = 0.01680 * M_ks**2
+    return a + bX + cX2
 
 
 def getAbsMag(m, d):
@@ -25,36 +38,45 @@ data = pd.read_csv(
     r"C:\Users\Arnau\Documents\Bachelorproject\Python\mags_plx_combined_id.csv",
     delimiter=",",
 )
+data = data[data["Parallax"] != "--"]
+data = data[data["K_mag"] != "--"]
 
 
-oldKs = data["K_mag"].values
-oldplx = data["Parallax"].values
-oldsource = data["source_obtained"].values.astype(str)
-oldID = data["ID"].values.astype(str)
-Ks = []
-plx = []
-source = []
-ID = []
-for i, k in enumerate(oldplx):
-    if k != "--":
-        if oldKs[i] != "--":
-            Ks.append(float(oldKs[i]))
-            plx.append(float(oldplx[i]))
-            source.append(str(oldsource[i]))
-            ID.append(str(oldID[i]))
+Ks = data["K_mag"].values.astype(float)
+plx = data["Parallax"].values.astype(float)
+
 
 d = [1000 / x for x in plx]
-print(np.shape(Ks))
 MKs = getAbsMag(Ks, d)
+MKs = MKs[(MKs >= 4.5) * (MKs < 9.5)]
 Mstar = getMass(MKs)
+Rstar = getRadius(MKs)
+
 
 plt.figure()
 plt.hist(MKs, bins=30)
 plt.title("Gaia/Simbad query")
 plt.xlabel("Absolute K magnitude")
+plt.text(7.7, 14, f"{len(MKs)} stars within domain", c="black")
+
+
+fig = plt.figure()
+# fig.title("Gaia/Simbad query")
+
+fig.add_subplot(1, 2, 1)
+plt.hist(Mstar, bins=20)
+plt.title("Gaia/Simbad query: Mass")
+plt.xlabel("Mass ($M_\odot$)")
+
+
+fig.add_subplot(1, 2, 2)
+plt.hist(Rstar, bins=20)
+plt.title("Gaia/Simbad query: Radius")
+plt.xlabel("Radius ($R_\odot$)")
+
 
 plt.figure()
-plt.hist(Mstar, bins=30)
-plt.title("Gaia/Simbad query")
+plt.plot(Mstar, Rstar, "bo")
+plt.ylabel("Radius ($R_\odot$)")
 plt.xlabel("Mass ($M_\odot$)")
 plt.show()
